978a6e1ad0f940123f9bd093cbc247a9f0048ae1
iambinaytiwari <binaytiwari6@gmail.com>
2025-06-27 07:19:27 +0545

    github-action: ci: update
    Change-Id: I633305fe71af83eebc98e9042e451612c24139f1

--- build.sh
+++ build.sh
@@ -1,42 +1,46 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+set -e -x
 
 # Kernel Source
-KERNEL_SOURCE="https://github.com/iambinaytiwari/android_kernel_samsung_m51"
-KERNEL_BRANCH="lineage-21"
-KERNEL_DEFCONFIG="m51_defconfig"
+KERNEL_SOURCE=https://github.com/iambinaytiwari/android_kernel_samsung_m51.git
+KERNEL_BRANCH=15.0
+KERNEL_DEFCONFIG=m51_defconfig
 
 # Prebuilt Clang Toolchain (AOSP)
-CLANG_URL="https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r547379.tar.gz"
+CLANG_URL=https://android.googlesource.com/platform//prebuilts/clang/host/linux-x86/+archive/4c6fbc28d3b078a5308894fc175f962bb26a5718/clang-r383902b1.tar.gz
 
 # Prebuilt GCC Utilities (AOSP)
-GCC_x64="https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9"
-GCC_x32="https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9"
+GCC_x64=https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
+GCC_x32=https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9
+GCC_BRANCH=master-kernel-build-2021
 
 # Setup make Command
 make_fun() {
-	make O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 \
-		CLANG_TRIPLE=aarch64-linux-gnu- \
-		CROSS_COMPILE=aarch64-linux-android- \
-                CROSS_COMPILE_ARM32=arm-linux-androideabi- "$@"
+  make O=out ARCH=arm64 \
+       CC=clang HOSTCC=clang LLVM=1 \
+       CLANG_TRIPLE=aarch64-linux-gnu- \
+       CROSS_COMPILE=aarch64-linux-android- \
+       CROSS_COMPILE_COMPAT=arm-linux-androideabi- "$@"
 }
 
 # Work Path
-WORK="${HOME}/work"
+WORK=${HOME}/work
 
 # Kernel Folder Name
-KERNEL="myKernel"
+KERNEL=android-kernel
 
 # Kernel Source Path
-KERNEL_SRC="${WORK}/${KERNEL}"
+KERNEL_SRC=${WORK}/${KERNEL}
 
 # Prepare Directory
 mkdir -p "${WORK}"
 cd "${WORK}" || exit 1
 
 # Cloning all the Necessary files
-if [ ! -d clang ]; then mkdir clang && curl "${CLANG_URL}" -RLO && tar -C clang/ -xf clang-*.tar.gz; fi
-[ ! -d x64 ] && git clone --depth=1 "${GCC_x64}" ./x64
-[ ! -d x32 ] && git clone --depth=1 "${GCC_x32}" ./x32
+if [ ! -d clang ]; then mkdir clang && curl -Lsq "${CLANG_URL}" -o clang.tgz && tar -xzf clang.tgz -C clang; fi
+[ ! -d x64 ] && git clone --depth=1 "${GCC_x64}" -b "${GCC_BRANCH}" ./x64
+[ ! -d x32 ] && git clone --depth=1 "${GCC_x32}" -b "${GCC_BRANCH}" ./x32
 [ ! -d "${KERNEL}" ] && git clone --depth=1 "${KERNEL_SOURCE}" -b "${KERNEL_BRANCH}" "${KERNEL}"
 
 # Setting Toolchain Path
@@ -45,10 +49,14 @@ PATH="${WORK}/clang/bin:${WORK}/x64/bin:${WORK}/x32/bin:/bin"
 # Enter Kernel root directory
 cd "${KERNEL_SRC}" || exit 1
 
-# KernelSU - Disable if not building one.
-curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next-susfs/kernel/setup.sh" | bash -s next-susfs
+# Export Additional Information, change it all according to your wants.
+export KBUILD_BUILD_HOST="${KERNEL}"
+export KBUILD_BUILD_USER="iambinaytiwari"
+
+# Install KernelSu-Next
+curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -
 
 # Start Compiling Kernel
 make_fun "${KERNEL_DEFCONFIG}"
-make_fun -j"$(nproc --all)" 2>&1 | tee build.log
+make_fun -j"$(nproc --all)" 2>&1 | tee build.log || exit 1
 tools/mkdtimg create out/arch/arm64/boot/dtbo.img --page_size=4096 $(find out/arch -name "*.dtbo")
